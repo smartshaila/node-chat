8f0bbca2b26b8635d82376dba19a492e6168ad02
smartshaila <smartshaila@gmail.com>
2015-10-18 22:19:36 -0400

    Usernames! Who's Online? Gasp.

--- index.js
+++ index.js
@@ -6,15 +6,23 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
+var user_array = [];
+
 io.on('connection', function(socket){
-   console.log('a user connected');
-   socket.on('disconnect', function(){
-      io.emit('signoff', 'a user disconnected');
-      console.log('a user disconnected');
-   });
-   socket.on('chat message', function(msg){
-      io.emit('chat message', msg);
-   });
+    socket.on('set username', function(username) {
+        socket.username = username;
+        user_array.push(username);
+        io.emit('user list', user_array);
+        socket.broadcast.emit('status', username + ' has connected');
+    });
+    socket.on('disconnect', function(){
+        user_array.splice(user_array.indexOf(socket.username), 1);
+        io.emit('user list', user_array);
+        io.emit('status', socket.username + ' has disconnected');
+    });
+    socket.on('chat message', function(msg){
+        io.emit('chat message', socket.username + ': ' + msg);
+    });
 });
 
 http.listen(3000, function(){
